6eb78ed6d26a0e2e09bbe15ef038570fd4d94c67
Max Khrichtchatyi <maxim.khrichtchatyi@gmail.com>
2021-01-10 03:17:39 +0600

    feat: 🐻 extend while loop

--- src/general/while_loop.rs
+++ src/general/while_loop.rs
@@ -3,8 +3,23 @@
 pub fn while_loop() {
     let mut incremented_value: i8 = 1;
 
-    while incremented_value <= 100 {
+    while incremented_value <= 10 {
+        if incremented_value == 4 {
+            incremented_value += 1;
+            continue;
+        }
+
         println!("Incremented value = {}", incremented_value);
         incremented_value += 1;
+    }
+
+    let mut incremented_value_for_loop: i8 = 1;
+    loop { // while true
+        println!("Incremented value for loop = {}", incremented_value_for_loop);
+        incremented_value_for_loop += 1;
+
+        if incremented_value_for_loop == 10 {
+            break;
+        }
     }
 }
