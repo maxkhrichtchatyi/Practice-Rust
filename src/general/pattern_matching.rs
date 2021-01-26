44a6ebebc3b61111154900a806cc14439ad6c536
Max Khrichtchatyi <maxim.khrichtchatyi@gmail.com>
2021-01-26 23:55:24 +0600

    feat: 🐻 extend Pattern Matching

--- src/general/pattern_matching.rs
+++ src/general/pattern_matching.rs
@@ -17,4 +17,13 @@ pub fn pattern_matching() {
     for x in 0..13 {
         println!("{}: I have {} oranges", x, how_many(x));
     }
+
+    let point = (3, 4);
+
+    match point {
+        (0, 0) => println!("origin"),
+        (0, y) => println!("x axis, y = {}", y),
+        (x, 0) => println!("x = {}, y axis", x),
+        (x, y) => println!("x = {}, y = {}", x, y)
+    }
 }
