86029411dd3703d78b9b408091dd02284567eac8
Max Khrichtchatyi <maxim.khrichtchatyi@gmail.com>
2021-01-27 01:23:11 +0600

    feat: 🐻 extend Pattern Matching

--- src/general/pattern_matching.rs
+++ src/general/pattern_matching.rs
@@ -11,6 +11,14 @@ fn how_many(x: i32) -> &'static str {
     }
 }
 
+enum Color {
+    Red,
+    Green,
+    Blue,
+    RGBColor(u8, u8, u8),
+    CMYKColor { cyan: u8, magenta: u8, yellow: u8, black: u8 },
+}
+
 pub fn pattern_matching() {
     println!("\n>>{}\n", "Pattern matching");
 
@@ -25,5 +33,34 @@ pub fn pattern_matching() {
         (0, y) => println!("x axis, y = {}", y),
         (x, 0) => println!("x = {}, y axis", x),
         (x, y) => println!("x = {}, y = {}", x, y)
+    }
+
+    let color_regular: Color = Color::Red;
+    let color_rgb: Color = Color::RGBColor(255, 255, 255);
+    let color_cmyk: Color = Color::CMYKColor {
+        cyan: 0,
+        magenta: 0,
+        yellow: 0,
+        black: 0,
+    };
+
+    match color_cmyk {
+        Color::Red => {
+            println!("Color Red");
+        }
+        Color::Blue => {
+            println!("Color Blue");
+        }
+        Color::Green => {
+            println!("Color Green");
+        }
+        Color::RGBColor(255, 0, 0)
+        | Color::CMYKColor { black: 0, .. } => {
+            println!("Color Black");
+        }
+        Color::RGBColor(r, g, b) => {
+            println!("RGB Color is {},{},{}", r, g, b);
+        }
+        _ => ()
     }
 }
