d2b9cc7de7e5e1df5863200f26d827ddde4d254a
Max Khrichtchatyi <maxim.khrichtchatyi@gmail.com>
2021-01-29 12:39:59 +0600

    feat: 🐻 extend Generics

--- src/general/generics.rs
+++ src/general/generics.rs
@@ -6,6 +6,11 @@ struct Point<T, V> {
     y: V,
 }
 
+struct Line<T, V> {
+    start: Point<T, V>,
+    end: Point<T, V>,
+}
+
 pub fn generics() {
     println!("\n>>{}\n", "Generics");
 
@@ -27,4 +32,21 @@ pub fn generics() {
     println!("a x={} y={}", a.x, a.y);
     println!("b x={} y={}", b.x, b.y);
     println!("c x={} y={}", c.x, c.y);
+
+    let point_line_a: Point<f32, f32> = Point {
+        x: 3.0,
+        y: 4.0,
+    };
+
+    let point_line_b: Point<f32, f32> = Point {
+        x: 13f32,
+        y: 14.0,
+    };
+
+    let my_line = Line {
+        start: point_line_a,
+        end: point_line_b,
+    };
+
+    println!("my_line start x={} y={}, end x={} y={}", my_line.start.x, my_line.start.y, my_line.end.x, my_line.end.y);
 }
