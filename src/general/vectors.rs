d9dbb4661418c8516e8229bd0ced0121017731bf
Max Khrichtchatyi <maxim.khrichtchatyi@gmail.com>
2021-02-01 23:55:53 +0600

    feat: 🐻 extend Vectors

--- src/general/vectors.rs
+++ src/general/vectors.rs
@@ -18,4 +18,24 @@ pub fn vectors() {
     // isize usize
     let idx: usize = 0;
     println!("first element a[0] = {}", a[idx]);
+
+    // Option
+    match a.get(6) {
+        Some(x) => println!("a[6] = {}", x),
+        None => println!("Error, no such element")
+    }
+
+    for x in &a {
+        println!("{}", x);
+    }
+
+    let last_element = a.pop(); // Option
+    println!("last_element = {:?}", last_element);
+    println!("a = {:?}", a);
+
+    // Iterate Some that returns Option
+    // yield
+    while let Some(x) = a.pop() {
+        println!("{}", x);
+    }
 }
